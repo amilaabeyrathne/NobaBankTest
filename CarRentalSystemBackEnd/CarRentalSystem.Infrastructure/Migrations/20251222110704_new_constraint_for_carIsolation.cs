1f952bc158d4418953ff09c11ef16a29e06cc2bd
Amila Abeyrathne <amilaupeksha@gmail.com>
2025-12-22 12:11:29 +0100

    Added new migrations

---
+++ CarRentalSystemBackEnd/CarRentalSystem.Infrastructure/Migrations/20251222110704_new_constraint_for_carIsolation.cs
@@ -0,0 +1,38 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace CarRentalSystem.Infrastructure.Migrations
+{
+    /// <inheritdoc />
+    public partial class new_constraint_for_carIsolation : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropIndex(
+               name: "IX_Reservations_CarId",
+               table: "Reservations");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Reservations_CarId",
+                table: "Reservations",
+                column: "CarId");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropIndex(
+                name: "IX_Reservations_CarId",
+                table: "Reservations");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Reservations_CarId",
+                table: "Reservations",
+                column: "CarId",
+                unique: true,
+                filter: "IsReturned = 0");
+        }
+    }
+}
---
+++ CarRentalSystemBackEnd/CarRentalSystem.Infrastructure/Migrations/20251222110824_AddActiveReservationUniqueIndex.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace CarRentalSystem.Infrastructure.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddActiveReservationUniqueIndex : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+           migrationBuilder.CreateIndex(
+           name: "UX_Reservations_ActiveCar",
+           table: "Reservations",
+           column: "CarId",
+           unique: true,
+           filter: "IsReturned = 0");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+           migrationBuilder.DropIndex(
+           name: "UX_Reservations_ActiveCar",
+           table: "Reservations");
+        }
+    }
+}
