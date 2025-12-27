ee9ae3222925a0a757b09c9b4535cd85a810258b
Amila Abeyrathne <amilaupeksha@gmail.com>
2025-12-27 16:35:11 +0100

    moved factory to domain layer

---
+++ CarRentalSystemBackEnd/CarRentalSystem.Domain/Factory/IPricingStrategyFactory.cs
@@ -0,0 +1,10 @@
+﻿using CarRentalSystem.Domain.PricingStrategies;
+using CarRentalSystem.Domain.Primitives;
+
+namespace CarRentalSystem.Domain.Factory
+{
+    public interface IPricingStrategyFactory
+    {
+        IRentalPricingStrategy GetStrategy(CarPricingStrategy pricingStrategy);
+    }
+}
---
+++ CarRentalSystemBackEnd/CarRentalSystem.Domain/Factory/PricingStrategyFactory.cs
@@ -0,0 +1,23 @@
+﻿using CarRentalSystem.Domain.PricingStrategies;
+using CarRentalSystem.Domain.Primitives;
+
+namespace CarRentalSystem.Domain.Factory
+{
+    public class PricingStrPricingStrategyFactoryategy : IPricingStrategyFactory
+    {
+        private readonly Dictionary<CarPricingStrategy, IRentalPricingStrategy> _strategies = new()
+        {
+            { CarPricingStrategy.SmallCar, new SmallCarPricingStrategy() },
+            { CarPricingStrategy.Combi, new CombiPricingStrategy() }, 
+            {CarPricingStrategy.Truck, new TruckPricingStrategy() }
+        };
+
+        public IRentalPricingStrategy GetStrategy(CarPricingStrategy pricingStrategy)
+        {
+            if (_strategies.TryGetValue(pricingStrategy, out var strategy))
+                return strategy;
+
+            throw new NotSupportedException($"No pricing strategy defined for category  {pricingStrategy}");
+        }
+    }
+}
